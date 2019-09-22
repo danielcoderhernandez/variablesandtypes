9551d3cc2119510e16cc89f5394eac535179f60f
Daniel Hernandez <dhernandez43@cnm.edu>
2019-09-21 20:55:52 -0600

    test

--- variables-and-data.js
+++ variables-and-data.js
@@ -1,12 +1,53 @@
 /*
  * Variables, Data Types, and Typing
  */
+
+/*
+*Variables
+ */
+
+let wordData;
+
+wordData = "Word List";
+
+console.log(wordData);
+
+let subTotal = 100;
+let tax = subTotal * 0.07;
+let totalWithTax = tax + subTotal;
+
+/*
+*Data Types
+ */
 let obj = {
 	key1: "value",
-	key2: 3,
+	key: 4,
+	boolean: true,
 	obj2: {
-		obj2Key1:"Internal Object Value",
-	},
+		obj2key1: "Internal Object Value",
+	}
 };
 
-console.log(obj.obj2.obj2Key1);
+console.log (obj.obj2.obj2key1);
+
+/*
+*Typing Systems
+ */
+let var1 = 2;
+let var2 = 5;
+let result = var1 + var2;
+
+console.log(result);
+
+/*
+*operators
+ */
+let add = 1 + 2;
+let sub = 2 - 1;
+let mult = 2*4;
+let division = 4 / 2;
+let mod = 5 % 2;
+
+let string = "String 1 " + "String 2";
+
+console.log(string);
